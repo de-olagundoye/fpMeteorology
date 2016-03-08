1cd2ddabaa3dd5c11df03b88e0155ecf4b872211
De Olagundoye <de.olagundoye@gmail.com>
2016-03-08 18:08:06 -0500

    creates drop down menus for filter params

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('fpMeteorology', []);
+
+$(document).ready(function () {
+  $('#drop-down li').hover(
+  function () {
+      //show submenu
+      $('ul', this).show();
+  }, function () {
+      //hide submenu
+      $('ul', this).hide();
+  });
+});
